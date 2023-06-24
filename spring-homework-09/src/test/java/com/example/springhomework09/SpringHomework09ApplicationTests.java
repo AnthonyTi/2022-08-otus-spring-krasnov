557ebd9e6acf6d364497b609e1f75c08f58bc954
Краснов Антон Алексеевич <anton.a.krasnov@domrf.ru>
2023-06-24 19:17:26 +0300

    Коммит 9 задания.

---
+++ spring-homework-09/src/test/java/com/example/springhomework09/SpringHomework09ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springhomework09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHomework09ApplicationTests {
+
+
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
