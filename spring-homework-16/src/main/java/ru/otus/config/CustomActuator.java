fcfff09c6fa37c59fc8fbcb0a5f13e27d9700d10
Краснов Антон Алексеевич <anton.a.krasnov@domrf.ru>
2023-10-16 14:52:20 +0300

    Добавлен кастомный checkstatus для монги

--- spring-homework-16/src/main/java/ru/otus/config/CustomActuator.java
+++ spring-homework-16/src/main/java/ru/otus/config/CustomActuator.java
@@ -1,17 +1,30 @@
 package ru.otus.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class CustomActuator implements HealthIndicator {
+
+    private final MongoTemplate mongoTemplate;
 
     @Override
     public Health health() {
-        return Health.up().build();
+
+        try {
+            mongoTemplate.executeCommand("{ serverStatus: 1 }");
+            return Health.up().build();
+        }
+        catch(Exception e) {
+            log.error(e.getMessage());
+            return Health.down().build();
+        }
     }
 
 }
